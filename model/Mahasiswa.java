5cacb604c08eb769de4b8a2bdc9ee730d95cdf12
irvanlazuardi <lazuardiphone@gmail.com>
2024-03-31 01:16:47 +0800

    Memanggil method dari Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ====================");
+  System.out.println("NPM  : 2210010313");
+  System.out.println("Nama : Irvan Lazuardi");
+  System.out.println("======================================");
+ }
+}
